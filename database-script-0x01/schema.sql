1616b64cc41ffda240c569d33215c43fb80600d0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-27 09:51:46 +0300

    User table

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,1 +1,10 @@
-s
+CREATE TABLE User (
+    user_id,
+    first_name varchar(),
+    last_name varchar(),
+    email varchar(),
+    password_hash varchar(),
+    phone_number varchar(),
+    role,
+    created_at,
+);
