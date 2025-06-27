00fdea4fb322d4fc8679268e8a04b8bd2e374b19
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-27 12:36:44 +0300

    Booking + Payment

--- database-script-0x02/seed.sql
+++ database-script-0x02/seed.sql
@@ -11,3 +11,16 @@ INSERT INTO Property (property_id, host_id, name, description, location, pricepe
 VALUES
   ('p1000000-p100-p100-p100-p10000000000', 'b1111111-b111-b111-b111-b11111111111', 'Seaside Villa', 'A beautiful villa by the sea.', 'Malibu, CA', 350.00),
   ('p2000000-p200-p200-p200-p20000000000', 'd2222222-d222-d222-d222-d22222222222', 'Mountain Cabin', 'Cozy cabin in the mountains.', 'Aspen, CO', 220.00);
+
+--Booking Table
+
+INSERT INTO Booking (booking_id, property_id, user_id, start_date, end_date, total_price, status)
+VALUES
+  ('b3000000-b300-b300-b300-b30000000000', 'p1000000-p100-p100-p100-p10000000000', 'a3333333-a333-a333-a333-a33333333333', '2025-07-01', '2025-07-05', 1400.00, 'confirmed'),
+  ('b4000000-b400-b400-b400-b40000000000', 'p2000000-p200-p200-p200-p20000000000', 'a3333333-a333-a333-a333-a33333333333', '2025-08-10', '2025-08-13', 660.00, 'pending');
+
+-- Payment
+INSERT INTO Payment (payment_id, booking_id, amount, payment_method)
+VALUES
+  ('pay5000000-pay5-pay5-pay5-pay50000000000', 'b3000000-b300-b300-b300-b30000000000', 1400.00, 'credit_card'),
+  ('pay6000000-pay6-pay6-pay6-pay60000000000', 'b4000000-b400-b400-b400-b40000000000', 660.00, 'paypal');
