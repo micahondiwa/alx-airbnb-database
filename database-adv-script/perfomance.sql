9b2f98de82adc1b527093406ab4160c732522dde
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-05 13:13:21 +0300

    Performance

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -49,3 +49,27 @@ CREATE INDEX idx_booking_id ON Booking(booking_id);
 CREATE INDEX idx_user_user_id ON User(user_id);
 CREATE INDEX idx_property_property_id ON Property(property_id);
 CREATE INDEX idx_payment_booking_id ON Payment(booking_id);
+
+
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+
+    u.user_id,
+    u.first_name,
+    u;last_name,
+
+    p.property_id,
+    p.name AS property_name,
+
+    pay.amount,
+    pay.payment_date,
+
+EXPLAIN ANALYZE
+FROM Booking b
+JOIN User u on b.user_id=user_id;
+JOIN Property p ON b.property_id=p.property_id;
+LEFT JOIN Payment pay ON b.booking_id=pay.booking_id;
