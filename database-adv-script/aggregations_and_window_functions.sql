e7f4bf2919c8c57c008ec16a91f7dbb5efee6def
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-05 11:08:07 +0300

    Aggregate

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,3 +3,5 @@ SELECT User_id
 COUNT(*) AS total_bookings
 FROM Booking
 GROUP BY User_id;
+
+-- Using a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
