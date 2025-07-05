43854d57d02f66d2964ea27ade79295ae89d16d6
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-05 11:28:10 +0300

    Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,10 +9,17 @@ ON
     Booking.User_id = Users.User_id;
 
 -- A query using LEFT JOIN to retrieve all properties  and their reviews , including properties  that have no reviews
-SELECT Property.*, Review.*
-FROM Property
-LEFT JOIN Review ON Property.Property_id = Review.Property_id
-ORDER BY Property.property_id ASC;
+SELECT 
+    Property.*, Review.*
+FROM 
+    Property
+LEFT JOIN 
+    Review 
+ON 
+    Property.Property_id = Review.Property_id
+ORDER BY 
+    Property.property_id 
+ASC;
 
 -- A query using FULL OUTER JOIN to retrieve all users and bookings, even if user has no booking or a booking is not linked to a user.
 SELECT Users.*, Booking.*
