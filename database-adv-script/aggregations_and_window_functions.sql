387d13de747a1ba9e6d6001f1ff590ee1effba87
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-05 11:16:47 +0300

    Aggregate

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,7 +11,8 @@ SELECT
     Property_id AS property_id,
     Property.name AS property_name,
     COUNT(Booking_id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(Booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(Booking_id) DESC) AS row_number_rank
+    RANK() OVER (ORDER BY COUNT(Booking_id) DESC) AS booking_rank
 FROM 
     Property
 LEFT JOIN
