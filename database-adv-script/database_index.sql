affdb3a19bb62a162c3acb7af9b13d32631f9f0b
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-05 11:52:20 +0300

    Indexs

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,14 @@
+-- Index on User Table
+CREATE INDEX idx_user_id ON User(user_id)
+CREATE INDEX idx_email ON User(email)
+
+-- Indexes on Booking Table
+CREATE INDEX idx_booking_id on Booking(booking_id)
+CREATE INDEX idx_booking_user_id on Booking(user_id)
+CREATE INDEX idx_booking_property_id on Booking(property_id)
+CREATE INDEX idx_booking_status on Booking(status)
+CREATE INDEX idx_booking_start_date on Booking(start_date)
+
+-- Index on Property Table
+CREATE INDEX idx_property_id on Property(property_id)
+CREATE INDEX idx_property_host_id on Property(host_id)
