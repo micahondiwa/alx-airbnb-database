bfedffa2d65de8c660ea6cd30ed360d976e8a510
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-05 10:25:59 +0300

    Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,9 +5,9 @@ INNER JOIN User ON Booking.User_id=User.User_id
 
 SELECT Property *, Review *,
 FROM Property
-LEFT JOIN Review ON Property.Property-d=Review.Property_id
+LEFT JOIN Review ON Property_id=Review.Property_id
 
 
 SELECT *
 FROM Booking
-FULL OUTER JOIN User ON Booking.Booking_id=USER.User_id 
+FULL OUTER JOIN User ON Booking.Booking_id=User.User_id 
